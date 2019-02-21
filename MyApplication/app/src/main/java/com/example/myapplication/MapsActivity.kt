f49d85d37b75d78c28c76d32946487ff26df2f55
mustafaazzurr <mustafayounis27@gmail.com>
2019-02-21 22:24:25 +0300

    this is my first commit

--- MyApplication/app/src/main/java/com/example/myapplication/MapsActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -1,14 +1,25 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.Exception
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,6 +32,59 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        checkPermission()
+    }
+    val AccessLocation = 123
+
+    fun checkPermission(){
+
+        if (Build.VERSION.SDK_INT >= 23){
+
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),AccessLocation)
+                return
+            }
+        }
+        getUserLOcation()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+
+            AccessLocation ->{
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getUserLOcation()
+                }else{
+                    Toast.makeText(this,"accese location denied ",Toast.LENGTH_LONG).show()
+
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    }
+
+
+
+    fun getUserLOcation(){
+
+        Toast.makeText(this,"accese location now ",Toast.LENGTH_LONG).show()
+        //todo access user location
+
+        val mylocation = myLocationListiner()
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            1,
+            1f,
+            mylocation
+        )
+
+        val mythread = MyThread()
+        mythread.start()
     }
 
     /**
@@ -35,9 +99,57 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+    }
+
+    var myLocation:Location?=null
+    inner class myLocationListiner:LocationListener{
+        constructor(){
+            myLocation = Location("me")
+            myLocation!!.longitude = 0.0
+            myLocation!!.latitude = 0.0
+        }
+        override fun onLocationChanged(location: Location?) {
+
+            myLocation = location
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+        }
+
+
+    }
+
+    inner class MyThread:Thread{
+
+        constructor():super(){
+            //todo later see old location
+        }
+        override fun run(){
+
+            while (true) try {
+
+                runOnUiThread{
+                    mMap!!.clear()
+                    val sydney = LatLng(myLocation!!.latitude,myLocation!!.longitude)
+                    mMap.addMarker(MarkerOptions()
+                        .position(sydney)
+                        .title("me"))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+                }
+
+
+
+                Thread.sleep(1000)
+            }catch (Ex:Exception){}
+        }
     }
 }
+
+
